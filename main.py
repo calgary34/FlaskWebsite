ebd937d24a8499c7f58541cb719b60af6d247137
Spot Sign <aaron34weston@gmail.com>
2020-12-22 01:39:18 +0000

    Removed all the random generator stuff, as they are now in seperate repos

--- main.py
+++ main.py
@@ -74,29 +74,7 @@ def delete_visits():
 @app.route("/datetime/")
 def date():
     return "<h1>The time is {}</h1>".format(datetime.datetime.now())
-@app.route('/random/float/')
-def randomfloat():
-    return render_template("rfloat.html")
-@app.route('/random/float/<low>/<high>/')
-def randomfloat_param(low,high):
-    return render_template("rfloat.html",low=low,high=high,rand=random.uniform(int(low), int(high)))
-@app.route('/random/letter/')
-def randomletter():
-    letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    return render_template("rletter.html",rand=random.choice(letters))
-@app.route('/random/')
-def randompage():
-    return render_template("random.html")
-@app.route('/random/int/<low>/<high>/')
-def randint_param(low,high):
-    return render_template("randint.html",low=low,high=high,rand=random.randint(int(low), int(high)))
-@app.route('/random/int/')
-def randint_no_param():
-    return render_template("randint.html")
-@app.route('/random/letter/greek/')
-def randomgreek():
-    letters=["α (alpha)","β (beta)","γ (gamma)","δ (delta)","ε (epsilon)","ζ (zeta)","η (eta)","θ (theta)","ι (iota)","κ (kappa)","λ (lambda)","μ (mu)","ν (nu)","ξ (xi)","ο (omicron)","π (pi)","ρ (rho)","σ (sigma)","τ (tau)","υ (upsilon)","φ (phi)","χ (chi)","ψ (psi)","ω (omega)"]
-    return render_template("rgreek.html",rand=random.choice(letters))
+
 @app.route('/math/pythagorean/')
 def pythagorean_triple():
     triple=pythagoreantriple(randint(1,10),randint(1,10))
